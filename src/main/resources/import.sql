d31927fb551c206c02b6c5de1890931dd1eb1a6b
Gab2904 <gabriel.r.martins@outlook.com>
2020-03-22 20:02:10 -0300

    Entrega exercicio previsao tempo

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1001, 'Segunda-Feira', 15, 25, 'Pancadas de Chuva na parte da tarde'); 
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1002, 'Terça-Feira', 18, 30, 'Ensolarado o dia inteiro'); 
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1003, 'Quarta-Feira', 18, 26, 'Ensolarado');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1004, 'Quinta-Feira', 17, 24, 'Ensolarado sem nuvens');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1005, 'Sexta-Feira', 15, 22, 'Nublado');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1006, 'Sabado', 15, 20, 'Nublado e chuvoso');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao) values (1007, 'Domingo', 16, 21, 'Nublado');
