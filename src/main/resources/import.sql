479fba0dc014f96e26ed0ac3d53155ba99861f6e
Gab2904 <gabriel.r.martins@outlook.com>
2020-03-28 16:28:33 -0300

    'Service'

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1001, 'Segunda-Feira', 15, 25, 'Pancadas de Chuva na parte da tarde', 86, '14:54 - 28/03/2020', '90°', '50°'); 
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1002, 'Terça-Feira', 18, 30, 'Ensolarado o dia inteiro', 40, '15:54 - 29/03/2020', '76°', '40°'); 
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1003, 'Quarta-Feira', 18, 26, 'Ensolarado', 25, '16:45 - 30/03/2020', '68°', '29°');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1004, 'Quinta-Feira', 17, 24, 'Ensolarado sem nuvens', 44, '10:34 - 01/04/2020', '54°', '30°');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1005, 'Sexta-Feira', 15, 22, 'Nublado', 50, '15:34 - 02/04/2020', '90°', '40°');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1006, 'Sabado', 15, 20, 'Nublado e chuvoso', 87, '17:43 - 03/04/2020', '70°', '40°');
+insert into temperatura (id, nome_semana, temp_min, temp_max, descricao, umidade, data_hora, latitude, longitude) values (1007, 'Domingo', 16, 21, 'Nublado', 78, '09:34 - 04/04/2020', '65°', '59°');
